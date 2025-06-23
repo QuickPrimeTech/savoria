fd8b1a74008219202f877e23a249cf1e5f231911
QuickPrimeTech <quickprimetech@gmail.com>
2025-06-23 19:27:32 +0300

    Added an opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,30 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Savoria | Fine Dining Restaurant",
   description: "Experience exquisite cuisine in an elegant atmosphere",
+  openGraph: {
+    title: "Savoria | Fine Dining Restaurant",
+    description: "Experience exquisite cuisine in an elegant atmosphere",
+    url: "https://savoria-swart.vercel.app",
+    siteName: "Savoria",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dhlyei79o/image/upload/v1750695743/OpenGraphImage_zaf8ol.png",
+        width: 1200,
+        height: 630,
+        alt: "Savoria Fine Dining Restaurant",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Savoria | Fine Dining Restaurant",
+    description: "Experience exquisite cuisine in an elegant atmosphere",
+    images: [
+      "https://res.cloudinary.com/dhlyei79o/image/upload/v1750695743/OpenGraphImage_zaf8ol.png",
+    ],
+  },
 };
 
 export default function RootLayout({
